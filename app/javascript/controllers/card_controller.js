ccfeacad2480e26331de06382b15a79a7b271ae8
frank <frank@Ubuntu-Dev.myguest.virtualbox.org>
2024-06-01 22:15:39 +0200

    added the housing cards and added the shadow effect when hovered

---
+++ app/javascript/controllers/card_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.textContent = "Hello World!"
+  }
+
+
+  static targets = ["card"]
+
+  connect() {
+    this.cardTarget.addEventListener("mouseover",this.changeShadow.bind(this,"shadow"));
+    this.cardTarget.addEventListener("mouseleave", this.changeShadow.bind(this, "shadow-sm"));
+  }
+
+  changeShadow(shadow) {
+    this.cardTarget.classList.remove("shadow", "shadow-sm");
+    this.cardTarget.classList.add(shadow);
+  }
+}
