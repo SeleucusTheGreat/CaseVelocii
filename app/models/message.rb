de0621510db99e567929c8e238d2d0e37a333b57
frank <frank@Ubuntu-Dev.myguest.virtualbox.org>
2024-06-20 20:52:05 +0200

    added the message on the userpage

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ApplicationRecord
     belongs_to :sender, class_name: "User"
     belongs_to :receiver, class_name: "User"
     belongs_to :post, class_name: "Post"
-  
     validates :content, presence: true
 end
   
