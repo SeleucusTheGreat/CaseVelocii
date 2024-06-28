e5f051299bbb983088663db23500326c31a7b8f7
frank <frank@Ubuntu-Dev.myguest.virtualbox.org>
2024-06-28 16:34:14 +0200

    commit before adding the map functions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "0ccb-93-42-66-190.ngrok-free.app"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
