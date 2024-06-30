aee1ac1c317fd1644b3c2110eaea05c1150a1e38
frank <frank@Ubuntu-Dev.myguest.virtualbox.org>
2024-06-30 15:06:21 +0200

    added a function to search from an address with a radius

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
- 
-  
-  
   def index
     @posts = Post.all
 
@@ -13,20 +10,18 @@ class HomeController < ApplicationController
       @posts = @posts.where(city: params[:city])
     end
 
-    if params[:num_rooms].present?
-      @posts = @posts.where("num_rooms >= ?", params[:num_rooms])
-    end
-
     if params[:square_feet].present?
       @posts = @posts.where(num_floors: params[:square_feet])
     end
-  end
 
+    if params[:address].present? && params[:radius].present?
+      @posts = @posts.near_address(params[:address], params[:radius])
+    end
+  end
 
   private
 
   def post_params
-    params.permit(:max_price, :price, :square_feet, :city, :num_floors, photos: [])
+    params.permit(:max_price, :price, :square_feet, :city, :address, :radius, :num_floors, photos: [])
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,10 @@ class Post < ApplicationRecord
     has_many :messages
     has_one :location, dependent: :destroy
     accepts_nested_attributes_for :location
+
+    scope :near_address, ->(address, radius) {
+        joins(:location).merge(Location.near(address, radius))
+      }
+
+    
 end
