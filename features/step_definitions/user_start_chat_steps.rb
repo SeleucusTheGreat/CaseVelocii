93be5990f11f6b1c15bbec6467f0a2039c6e544b
frank <frank@Ubuntu-Dev.myguest.virtualbox.org>
2024-07-15 16:17:36 +0200

    added another user story

--- features/step_definitions/user_start_chat_steps.rb
+++ features/step_definitions/user_start_chat_steps.rb
@@ -23,4 +23,59 @@
       end
   end
   
+  And ("I should see a Send Message and a text box") do
+    within('.col-md-8') do
+      expect(page).to have_selector('textarea#content')
+      expect(page).to have_button('Send Message')
+    end
+  end
+  
+  When('I write a new message') do
+    fill_in 'content', with: 'Is this house still available?'
+  end
+  
+  And ('I click on Send Message') do
+    click_button 'Send Message'
+  end
+  
+  Then('A new chat should be created between me and the House owner') do
+    @chat = Chat.last
+    expect(@chat.messages.last.content).to eq('Is this house still available?')
+    expect(@chat.owner).to eq(@user2)
+    expect(@chat.buyer).to eq(@user)
+  end
+  
+  When('I visit my User Page') do
+    visit userpage_path
+  end
+  
+  Then("A new chat should be visible") do
+    expect(page).to have_css('div.card.h-100')
+    within('div.card.h-100') do
+      expect(page).to have_css('div.card-body')
+      expect(page).to have_css('div.row.align-items-center')
+      expect(page).to have_css('div.col-md-3.text-center.User img.rounded-circle')
+      expect(page).to have_css('div.col-md-3.text-center.User h6', text: 'user2')
+      expect(page).to have_css('div.col-md-3.text-center.House h6', text: 'house')
+    end
+  end
+  
+  And ("it should display the other user's profile picture and name") do
+    within('div.card.h-100') do
+      expect(page).to have_css('div.col-md-3.text-center.User img.rounded-circle')
+      expect(page).to have_css('div.col-md-3.text-center.User h6', text: 'user2')
+    end
+  end
+  
+  And("it should show the associated post's image and title") do
+    within('div.col-md-3.text-center.House') do
+      expect(page).to have_css('img.rounded.mb-2')
+      expect(page).to have_css('h6.mb-0', text: 'house')
+    end
+  end
+  
+  
+  And("it should display the last message exchanged") do
+    expect(page).to have_content('Is this house still available?')
+  end
 
--- features/support/user_start_chat.feature
+++ features/support/user_start_chat.feature
@@ -10,11 +10,11 @@ Scenario: User starts a new chat from a house post
 	And I am logged in
 	When I click on a house post that interests me
 	Then I should see the details of the house post
-	And I should see a "Send Message" and a text box 
+	And I should see a Send Message and a text box
 	Then I write a new message
-	And  I click on “Send Message”
+	And  I click on Send Message
 	Then A new chat should be created between me and the House owner
-	When I visit my “User Page”
+	When I visit my User Page
 	Then A new chat should be visible
 	And it should display the other user's profile picture and name
 	And it should show the associated post's image and title
